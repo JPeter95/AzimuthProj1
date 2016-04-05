fc9e42aba4836433498fea2d38a269b1499a268a
thwalla1 <thwalla1@umbc.edu>
2016-04-05 00:27:51 -0400

    Added files via upload
    I guess I'm doing this correctly?
    
    Made a bunch of changes to the front-end. Should be ready to start integrating with php scripts on back-end.
    
    - Index.html should be depreciated. index.php is now a simple php script redirect to home.php which is our intended Home page.
    - Separated header and footer content into header.php and footer.php. These need to be included on every page intended for user navigation.
    - Article div content on each page now goes into <page>.html files for their respective <page>.php files to include between header and footer. This should enable easier user-facing edits without digging into php scripts.
    - Added a simple favicon icon (azimuth_rose.png), which I just threw together quickly in MS paint so no usage worries.
    - azimuth_architecture.png is for documentation. Use this to quick-reference how the front-end is organized if you're confused (hopefully not).

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<link rel="icon" type="image/png" href="azimuth_rose.png">
+  <head>
+   <title>Azimuth Course Advisor</title>
+    <link rel="stylesheet" type="text/css" href="azimuth.css">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <div class="title">
+          <h1>Azimuth</h1>
+          <h3>UMBC Academic Degree Course Advisor</h3>
+        </div>
+        <img src="digital-black.jpg" alt="Compass" class="header-image">
+      </div>
+      <div class="nav-bar">
+        <ul id="nav-bar">
+          <li><a href="home.php">Home</a></li>
+          <li><a href="get-started.php">Get Started</a></li>
+          <li><a href="about.php">About</a></li>
+        </ul>
+      </div>
---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+
+/*Pre-Load Section - NO HTML Output******************************************/
+
+//Pre-Load Variable Definition
+$pageTitle = "Azimuth &middot Home";
+
+/*Load Section - HTML Output OK**********************************************/
+
+//Output HTML Azimuth Header
+include_once('header.php');
+
+//Output HTML Azimuth Home Article
+include_once('home.html');
+
+//Output HTML Azimuth Footer
+include_once('footer.php');
+
+?>
